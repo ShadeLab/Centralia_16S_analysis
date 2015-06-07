25f3f1b74cc44b96b9b53e3b1ba5ffb47022bcb4
shlee1978 <shlee1978@github.com>
2015-06-06 22:35:52 -0400

    heatmap method update

--- Computing_commands/Heatmap/heatmap_original.r
+++ Computing_commands/Heatmap/heatmap_original.r
@@ -1,22 +1,25 @@
-
 library("RColorBrewer")
 library("RColorBrewer", lib.loc="~/R/win-library/3.1")
 my_palette <- colorRampPalette(c("green", "black", "red"))(n = 299)
 
 
 # Data table load
-phylum=read.csv("AVGED_Subsampling_otu_table_even73419_L2.csv", sep=",")
-phylum=read.csv("Phylum_AVG_MERGED.csv", sep=",")
-phylum=read.csv("Firmicutes.csv", sep=",")
-phylum=read.csv("WUF_Ceneven73419.txt", sep="\t")
-
+#phylum=read.csv("AVGED_Subsampling_otu_table_even73419_L2.csv", sep=",", header=TRUE)
+phylum=read.csv("Phylum_AVG_MERGED.csv", sep=",",  header=TRUE)
+#phylum=read.csv("Firmicutes.csv", sep=",",  header=TRUE)
+#phylum=read.csv("WUF_Ceneven73419.txt", sep="\t",  header=TRUE)
+#phylum=read.csv("BKM_OTUs.txt", sep="\t", header=TRUE)
 
 # sort by phylogenetic_affiliation.
+#phylum <- phylum[order(phylum$Phylogenetic_affiliation),]
 phylum <- phylum[order(phylum$Phylogenetic_affiliation),]
 
 # prepare data
 row.names(phylum) <- phylum$Phylogenetic_affiliation
 phylum <- phylum[,2:19]
+#row.names(phylum) <- phylum$Phylogenetic_affiliation
+#phylum <- phylum[,2:9]
+
 
 # prepare data matrix
 phylum_matrix <- data.matrix(phylum)
@@ -27,5 +30,6 @@ phylum_matrix <- data.matrix(phylum)
 
 library("gplots")
 library("gplots", lib.loc="~/R/win-library/3.1")
-phylum_heatmap <- heatmap.2(phylum_matrix, Rowv=NA, col=topo.colors(128), scale="column", key=TRUE, symkey=TRUE, density.info="none", trace="none", cexRow=0.6, margins=c(10,20))
-phylum_heatmap <- heatmap.2(phylum_matrix, col=my_palette, scale="row", key=TRUE, symkey=TRUE, density.info="none", trace="none", cexRow=0.4)
+phylum_heatmap <- heatmap.2(phylum_matrix, col=redgreen(256), scale="row", key=TRUE, symkey=TRUE, density.info="none", trace="none", cexRow=0.7, margins=c(5,15))
+#phylum_heatmap <- heatmap.2(phylum_matrix, col=redgreen(128), scale="row", key=TRUE, symkey=TRUE, density.info="none", trace="none", cexRow=0.7, margins=c(10,20))
+#phylum_heatmap <- heatmap.2(phylum_matrix, col=my_palette, scale="row", key=TRUE, symkey=TRUE, density.info="none", trace="none", cexRow=0.4)
