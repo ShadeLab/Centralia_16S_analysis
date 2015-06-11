c2a4a0bf2d3e79f2cdd2c1969e8e9f4addfd0598
ashleyshade <shade.ashley@gmail.com>
2015-06-11 13:24:12 -0400

    revise pch size and legend title

--- Data/ContextualData/CentraliaContextualDataGraph.R
+++ Data/ContextualData/CentraliaContextualDataGraph.R
@@ -10,23 +10,23 @@ library(ggplot2)
 library(reshape2)
 
 #use "melt" from the reshape2 package to make a redundant table for plotting chemistry
-data.long=melt(data, id.vars=c("SampleID", "Temperature", "Category", "Activity", "FireFront", "Elick", "Name2"))
+data.long=melt(data, id.vars=c("SampleID", "Temperature", "Category", "Activity", "FireFront", "Elick", "Name2", "PD_Avg", "Richness_Avg"))
 
-#make a gradient color palette
+#make a gradient color palette, note bias
 GnYlOrRd=colorRampPalette(colors=c("green", "yellow", "orange","red"), bias=2)
 
 
-#plot
+#plot, call figure "p"
 p=ggplot(data.long, aes(y=as.numeric(Temperature), x=value))+
   
   #add points layer
-  geom_point(aes(y=as.numeric(Temperature), x=value, shape=Category,size=1, color=as.numeric(Temperature)))+
+  geom_point(aes(y=as.numeric(Temperature), x=value, shape=Category,size=.7, color=as.numeric(Temperature)))+
   
   #set facet with 4 columns, make x-axes appropriate for each variable
-  facet_wrap(~variable, ncol=4, scales="free_x")+ 
+  facet_wrap(~variable, ncol=3, scales="free_x")+ 
   
   #set gradient for temperature and add gradient colorbar
-  scale_color_gradientn(colours=GnYlOrRd(20), guide="colorbar")+
+  scale_color_gradientn(colours=GnYlOrRd(20), guide="colorbar", guide_legend(title="Temperature"))+
   
   #omit the legend for the size of the points
   scale_size(guide=FALSE)+
@@ -36,3 +36,5 @@ p=ggplot(data.long, aes(y=as.numeric(Temperature), x=value))+
   
   #set a simple theme
   theme_bw()
+###
+p
