819025dca015571fb8ca8f504d783c712a921b1b
ashleyshade <shade.ashley@gmail.com>
2015-11-09 15:22:26 -0500

    update

--- Data/ContextualData/CentraliaContextualDataGraph.R
+++ Data/ContextualData/CentraliaContextualDataGraph.R
@@ -20,7 +20,7 @@ GnYlOrRd=colorRampPalette(colors=c("green", "yellow", "orange","red"), bias=2)
 p=ggplot(data.long, aes(y=as.numeric(Temperature), x=value))+
   
   #add points layer
-  geom_point(aes(y=as.numeric(Temperature), x=value, shape=Category,size=.7, color=as.numeric(Temperature)))+
+  geom_point(aes(y=as.numeric(Temperature), x=value, shape=Category,color=as.numeric(Temperature)))+
   
   #set facet with 4 columns, make x-axes appropriate for each variable
   facet_wrap(~variable, ncol=3, scales="free_x")+ 
@@ -29,7 +29,7 @@ p=ggplot(data.long, aes(y=as.numeric(Temperature), x=value))+
   scale_color_gradientn(colours=GnYlOrRd(20), guide="colorbar", guide_legend(title="Temperature"))+
   
   #omit the legend for the size of the points
-  scale_size(guide=FALSE)+
+  #scale_size(guide=FALSE)+
   
   #define the axis labels
   labs(y="Temperature (Celsius)", x=" ")+
