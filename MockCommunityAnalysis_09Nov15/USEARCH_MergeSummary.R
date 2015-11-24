f4b25ba90cd6cb221997c21e8e11d304bc789df6
ashleyshade <shade.ashley@gmail.com>
2015-11-24 15:05:03 -0500

    update workflows

---
+++ MockCommunityAnalysis_09Nov15/USEARCH_MergeSummary.R
@@ -0,0 +1,20 @@
+data=read.table("MergeResults_17nov15.txt", header=TRUE, sep="\t")
+
+library(ggplot2)
+
+#NOTE:  position_jitter is used to avoid jittering on the "y" axis, which would construe the data.
+
+p=ggplot(data, aes(y=as.numeric(Value), x=Descriptor))+
+  
+  #add points layer
+  geom_boxplot(aes(y=as.numeric(Value), x=Descriptor,color=Descriptor))+
+  
+  geom_point(aes(y=as.numeric(Value), x=Descriptor), position=position_jitter(w=0.25, h=0))+
+  
+  #set facet with 4 columns, make x-axes appropriate for each variable
+  facet_wrap(~Descriptor, ncol=4, scales=c("free"))+
+  
+  #omit the legend for the size of the points
+  scale_color_discrete(guide=FALSE)+
+  
+  theme(axis.text.x = element_blank())
